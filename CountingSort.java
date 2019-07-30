64dded5ea6fb00aaaed4303cf306c9828aed2321
Ronnie Song <ronsong@pdx.edu>
2019-07-30 00:14:34 -0700

    Create CountingSort.java

---
+++ CountingSort.java
@@ -0,0 +1,34 @@
+public class CountingSort implements IArraySort {
+    @Override
+    public int[] sort(int[] sourceArray) throws Exception {
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+        int maxValue = getMaxValue(arr);
+        return countingSort(arr, maxValue);
+    }
+
+    private int[] countingSort(int[] arr, int maxValue) {
+        int bucketLen = maxValue + 1;
+        int[] bucket = new int[bucketLen];
+        for (int value : arr) {
+            bucket[value]++;
+        }
+        int sortedIndex = 0;
+        for (int j = 0; j < bucketLen; j++) {
+            while (bucket[j] > 0) {
+                arr[sortedIndex++] = j;
+                bucket[j]--;
+            }
+        }
+        return arr;
+    }
+
+    private int getMaxValue(int[] arr) {
+        int maxValue = arr[0];
+        for (int value : arr) {
+            if (maxValue < value) {
+                maxValue = value;
+            }
+        }
+        return maxValue;
+    }
+}
