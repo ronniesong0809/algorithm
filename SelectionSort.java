fee380e3451adec27afceb48893d0b293326541a
Ronnie Song <ronsong@pdx.edu>
2019-07-30 00:06:09 -0700

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,24 @@
+public class SelectionSort implements IArraySort {
+
+    @Override
+    public int[] sort(int[] sourceArray) throws Exception {
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            if (i != min) {
+                int tmp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = tmp;
+            }
+        }
+        return arr;
+    }
+}
